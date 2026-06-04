f933a5a8373c712319a5cd58453ec270ce1403ca
zaeonizaeoni <zaeonizaeoni12@gmail.com>
2026-06-04 23:25:24 +0300

    Completing Extraction process with a problem within .contents i will figure it out later.

--- etl_process.py
+++ etl_process.py
@@ -9,9 +9,23 @@ url = 'https://worldpopulationreview.com/country-rankings/tea-consumption-by-cou
 db_name = 'Countries_consumption'
 table_name = 'tea_consumption_by_countries'
 db_connection = sqlite3.connect(db_name)
+df = pd.DataFrame(columns=['Country', 'Annual total tea consumption 2022 (TON)'])
 
-def extract():
-    pass
+def extract(url, df):
+    html_page = requests.get(url).text
+    table = BeautifulSoup(html_page, 'html.parser')
+    data = table.find_all('tbody')
+    rows = data[0].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col) != 0:
+            content = {
+                'Country': col[1].span.a.contents[0],
+                'tea_consumption_by_countries': col[2].contents[0]
+            }
+            df1 = pd.DataFrame(content, index=[0])
+            df = pd.concat([df,df1], ignore_index=True)
+    return df
 
 def transform():
     pass
@@ -24,3 +38,5 @@ def load_to_database():
 
 def log_progress():
     pass
+
+extract(url, df)
