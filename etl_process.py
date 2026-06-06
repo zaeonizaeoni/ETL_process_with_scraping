bc2472ffb83b698efc01366262247443a5d6a648
zaeonizaeoni <zaeonizaeoni12@gmail.com>
2026-06-06 07:40:25 +0300

    Building the logging information after import datetime library; to keep tracking progress

--- etl_process.py
+++ etl_process.py
@@ -3,7 +3,7 @@ import numpy as np
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
-
+from datetime import datetime
 
 url = 'https://worldpopulationreview.com/country-rankings/tea-consumption-by-country'
 db_name = 'Countries_consumption'
@@ -18,10 +18,10 @@ def extract(url, df):
     rows = data[0].find_all('tr')
     for row in rows:
         col = row.find_all('td')
-        if len(col) != 0:
+        if len(col) != 0 and col[2] is not None:
             content = {
                 'Country': col[1].span.a.contents[0],
-                'tea_consumption_by_countries': col[2].contents[0]
+                'Annual total tea consumption 2022 (TON)': col[2].contents[0]
             }
             df1 = pd.DataFrame(content, index=[0])
             df = pd.concat([df,df1], ignore_index=True)
@@ -36,7 +36,11 @@ def load_to_csv():
 def load_to_database():
     pass
 
-def log_progress():
-    pass
+def log_progress(message):
+    format_stamp = '%Y-%h-%d-%H:%M:%S'
+    now = datetime.now()
+    time_stamp = now.strftime(format_stamp)
+    with open('logging.txt', 'a') as f:
+        f.write(message + ', ' + time_stamp + '\n')
 
 extract(url, df)
